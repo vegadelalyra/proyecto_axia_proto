54e90bf51bd57ec9cfb790c76de52c5fcc3219a8
Vegadelalyra <lyradeperseo@gmail.com>
2024-11-29 05:36:24 -0500

    wip[vega]: raise dark/light mode toggle or page theme up to a context

--- frontend/src/pages/Gmao.jsx
+++ frontend/src/pages/Gmao.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Panel from '../components/panel';
+import Table from '../components/table';
 
 const Gmao = () => {
   return (
-    <>
+    <section style={{ display: 'flex', gap: '100px' }}>
       <Panel />
-    </>
+      <Table />
+    </section>
   );
 };
 
